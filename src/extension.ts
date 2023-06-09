0a996677ea0d357a11a23607969577753037cc4b
Ivan Kovarin <ivan.kovarin@itesoft.com>
2023-06-10 01:36:24 +0200

    Disable completion on text change, put in a separate branch.

--- src/extension.ts
+++ src/extension.ts
@@ -6,42 +6,15 @@ import JsonCompletionProvider from './providers/jsonCompletionProvider';
 import JsonDefinitionProvider from './providers/jsonDefinitionProvider';
 import JsonReferenceProvider from './providers/jsonReferenceProvider';
 import JsonRenameProvider from './providers/jsonRenameProvider';
-import { extractTextInQuotes } from './utils/utils';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// TODO finish rename provider, test in a big repo
-
 	context.subscriptions.push(vscode.languages.registerCompletionItemProvider("*", new JsonCompletionProvider(), '.'));
 	context.subscriptions.push(vscode.languages.registerDefinitionProvider('*', new JsonDefinitionProvider()));
-
-	// { language: 'json', pattern: '**​/package.json' }
 	context.subscriptions.push(vscode.languages.registerReferenceProvider('json', new JsonReferenceProvider()));
 	context.subscriptions.push(vscode.languages.registerRenameProvider('*', new JsonRenameProvider()));
-
-	// Open suggestions panel on text change inside quotes
-	context.subscriptions.push(
-		vscode.workspace.onDidChangeTextDocument(async (event) => {
-			const position = vscode.window.activeTextEditor?.selection.active;
-			if (position) {
-				const text = event.document.lineAt(position.line).text;
-				const textInQuotes = extractTextInQuotes(text, position!);
-
-				if (textInQuotes !== null) {
-					await vscode.commands.executeCommand(
-						'editor.action.triggerSuggest',
-						{
-							'triggerCharacter': '',
-							'triggerKind': vscode.CompletionTriggerKind.Invoke,
-							'position': position
-						}
-					);
-				}
-			}
-		})
-	);
 }
 
 // This method is called when your extension is deactivated
